4f2cddf9693a7010da118266a0a5b691763dea4a
akshay.k <akshay.k@gai.co.in>
2023-02-03 15:09:48 +0530

    navbar theming

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,32 +1,31 @@
-import * as React from 'react';
-import './Navbar.css';
-import AppBar from '@mui/material/AppBar';
-import { UserAuth } from './context/AuthContext';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import logo from './assets/logo.png';
-import { Link, useNavigate } from 'react-router-dom';
+import * as React from "react";
+import "./Navbar.css";
+import AppBar from "@mui/material/AppBar";
+import { UserAuth } from "./context/AuthContext";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import logo from "./assets/logo.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-
   const { user, logOut } = UserAuth();
   const navigate = useNavigate();
 
   const handleSignOut = async () => {
-    try { 
+    try {
       await logOut();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -34,11 +33,11 @@ const Navbar = () => {
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleprofile = () =>{
-    navigate("/account")
+  const handleprofile = () => {
+    navigate("/account");
   };
-  const handlelogo = () =>{
-    navigate("/")
+  const handlelogo = () => {
+    navigate("/");
   };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
@@ -51,161 +50,206 @@ const Navbar = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-  React.useEffect(()=> {
-    console.log('user',user)
-  })
+  React.useEffect(() => {
+    console.log("user", user);
+  });
   return (
     <AppBar position="static">
       {user && (
-      <>
-      <Container maxWidth="xxxl">
-        <Toolbar  sx={{ padding : 0}}>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              <MenuItem key='Home'>
-                  <Typography href="/">Home</Typography>
-              </MenuItem>
-              <MenuItem key='Donate book'>
-                  <Typography href="/donate">Donate Book</Typography>
-              </MenuItem>
-              <MenuItem key='Profile'>
-                  <Typography href="/account">Profile</Typography>
-              </MenuItem>
-            </Menu>
-            <Link onClick={handlelogo}>
-              <Box
-                className='mobile'
-                component="img"
-                sx={{ height: 40, marginTop: 0.5 }}
-                alt="Logo"
-                src={logo}
-              />
-            </Link>
-          </Box>
-          <Link onClick={handlelogo}>
-            <Box
-              className='desktop'
-              component="img"
-              sx={{ height: 54 }}
-              onClick={handlelogo}
-              alt="Logo"
-              src={logo}
-            />
-          </Link>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              <MenuItem key='Home'>
-                  <Typography variant="h5"
+        <>
+          <Container maxWidth="xxxl">
+            <Toolbar sx={{ padding: 0 }}>
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  <MenuItem key="Home">
+                    <Typography>
+                      {" "}
+                      <Link to="/" underline="none">
+                        Home
+                      </Link>
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem key="Donate book">
+                    <Link to="/donate">
+                      {" "}
+                      <Typography>Donate Book</Typography>
+                    </Link>
+                  </MenuItem>
+                  <MenuItem key="Profile">
+                    <Link to="/account">
+                      {" "}
+                      <Typography href="/account">Profile</Typography>
+                    </Link>
+                  </MenuItem>
+                </Menu>
+                <Link onClick={handlelogo}>
+                  <Box
+                    className="mobile"
+                    component="img"
+                    sx={{ height: 40, marginTop: 0.5 }}
+                    alt="Logo"
+                    src={logo}
+                  />
+                </Link>
+              </Box>
+              <Link onClick={handlelogo}>
+                <Box
+                  className="desktop"
+                  component="img"
+                  sx={{ height: 54 }}
+                  onClick={handlelogo}
+                  alt="Logo"
+                  src={logo}
+                />
+              </Link>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                <MenuItem key="Home">
+                  <Typography
+                    variant="h5"
                     noWrap
-                    component="a" href="/" sx={{
+                    component="a"
+                    sx={{
                       mr: 2,
                       display: "block",
-                      color: '#ffff',
-                      textDecoration: 'none',
-                    }}>
-                    Home
+                      color: "#ffff",
+                      textDecoration: "none",
+                    }}
+                  >
+                    <Link
+                      to="/"
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      Home
+                    </Link>
                   </Typography>
-              </MenuItem>
-              <MenuItem key='Donate book'>
-                  <Typography variant="h5"
-                    noWrap
-                    component="a" href="/donate" sx={{
-                    mr: 2,
-                    display: "block",
-                    color: '#ffff',
-                    textDecoration: 'none',
-                   }}>
-                    Donate Book
-                  </Typography>
-              </MenuItem>
-              <MenuItem key='Profile'>
-                  <Typography variant="h5"
-                      noWrap
-                      component="a" href="/account" sx={{
-                      mr: 2,
-                      display:"block",
-                      color: '#ffff',
-                      textDecoration: 'none',
-                      }}>
-                      Profile
-                  </Typography>
-              </MenuItem>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip>
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt={(user.email) ? user.email.charAt(0).toUpperCase() : '' } src={(user.photoURL) ? user.photoURL : '/alttext'}  />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              { user.displayName &&
-              <MenuItem onClick={handleprofile}>
-                <Typography sx={{
-              fontWeight: 900}}>Hi {user.displayName}</Typography>
-              </MenuItem> 
-              }
-              { (!user.displayName && user.email) &&
-              <MenuItem onClick={handleprofile}>
-               <Typography sx={{
-              fontWeight: 900}}>
-               Login with {user.email}
-               </Typography>
-               
-              </MenuItem>
-               }
-              <MenuItem onClick={handleSignOut}>
-                  <Typography>Logout</Typography>
                 </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-     </>
+                <MenuItem key="Donate book">
+                  <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    sx={{
+                      mr: 2,
+                      display: "block",
+                      color: "#ffff",
+                      textDecoration: "none",
+                    }}
+                  >
+                    {" "}
+                    <Link
+                      to="/donate"
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      Donate Book
+                    </Link>
+                  </Typography>
+                </MenuItem>
+                <MenuItem key="Profile">
+                  <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    sx={{
+                      mr: 2,
+                      display: "block",
+                      color: "#ffff",
+                      textDecoration: "none",
+                    }}
+                  >
+                    <Link
+                      to="/account"
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      Profile
+                    </Link>
+                  </Typography>
+                </MenuItem>
+              </Box>
+
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip>
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt={user.email ? user.email.charAt(0).toUpperCase() : ""}
+                      src={user.photoURL ? user.photoURL : "/alttext"}
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {user.displayName && (
+                    <MenuItem onClick={handleprofile}>
+                      <Typography
+                        sx={{
+                          fontWeight: 900,
+                        }}
+                      >
+                        Hi {user.displayName}
+                      </Typography>
+                    </MenuItem>
+                  )}
+                  {!user.displayName && user.email && (
+                    <MenuItem onClick={handleprofile}>
+                      <Typography
+                        sx={{
+                          fontWeight: 900,
+                        }}
+                      >
+                        Login with {user.email}
+                      </Typography>
+                    </MenuItem>
+                  )}
+                  <MenuItem onClick={handleSignOut}>
+                    <Typography>Logout</Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </>
       )}
-   </AppBar>
+    </AppBar>
   );
 };
 export default Navbar;
